f49078e32f866e5d264ea2abfeacdf8dd098f5b6
marc <jeanaulivan@gmail.com>
2026-03-25 15:03:13 +0100

    debug_add_layout

---
+++ src/app/debug/layout.tsx
@@ -0,0 +1,14 @@
+// app/debug/layout.tsx
+export default function DebugLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fr">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
