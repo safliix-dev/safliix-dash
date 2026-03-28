71334d764148bcd3f0c9ad4b1953bc939a8c2b29
marc <jeanaulivan@gmail.com>
2026-03-28 14:29:26 +0100

    add_useJobSocket_to_film_page

--- src/lib/socket/socket-client.ts
+++ src/lib/socket/socket-client.ts
@@ -1,7 +1,7 @@
 import { io, Socket } from "socket.io-client";
 
 // L'URL de votre backend NestJS
-const SOCKET_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+const SOCKET_URL = process.env.NEXT_PUBLIC_API_URL || "https://dashboard-api.safliix.com";
 
 /**
  * Instance unique du socket pour le namespace video-progress.
