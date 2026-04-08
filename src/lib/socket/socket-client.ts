5fc33b5b2c2284707ade3cc25eb77ed50294cfcc
marc <jeanaulivan@gmail.com>
2026-04-08 13:39:02 +0100

    debug_socket_connectionçbug

--- src/lib/socket/socket-client.ts
+++ src/lib/socket/socket-client.ts
@@ -3,6 +3,8 @@ import { io, Socket } from "socket.io-client";
 // L'URL de votre backend NestJS
 const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || "https://svr-processor.safliix.com";
 
+console.log("socket url:"+SOCKET_URL)
+
 /**
  * Instance unique du socket pour le namespace /processing.
  * autoConnect est à false car nous gérons la connexion manuellement après l'authentification.
