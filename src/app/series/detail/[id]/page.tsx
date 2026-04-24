cc968804c7d6770d0a3805fb5fcec7a11686cd81
marc <jeanaulivan@gmail.com>
2026-04-24 16:45:54 +0100

    add_some_refactoring

--- src/app/series/detail/[id]/page.tsx
+++ src/app/series/detail/[id]/page.tsx
@@ -1,14 +1,7 @@
-import { PageParamProps } from "@/types/utils";
 import SeriesDetailClient from "./client";
-import { seriesApi } from "@/lib/api/series";
-import { cookies } from "next/headers";
 
-export default async function Page({ params }: PageParamProps) {
-  const { id } = await params;
-  const cookieStore = await cookies();
-  const accessToken = cookieStore.get("accessToken")?.value;
-  const detail = await seriesApi.detail(id, accessToken);
-  const seasons = detail.seasons?.map((s) => ({ id: s.id, number: String(s.numero ?? s.id) })) ?? [];
+export default async function Page() {
+  
 
-  return <SeriesDetailClient id={id} seasons={seasons} detail={detail} />;
+  return <SeriesDetailClient  />;
 }
